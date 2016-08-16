8b960ba51bfc3fc58fd11601c76438721ee857e8
Thomas Boerger <tboerger@suse.de>
2016-08-16 12:42:43 +0200

    Made token optional

--- cmd/handle.go
+++ cmd/handle.go
@@ -12,9 +12,13 @@ import (
 type HandleFunc func(c *cli.Context, client umschlag.ClientAPI) error
 
 // Handle wraps the command function handler.
-func Handle(c *cli.Context, fn HandleFunc) {
-	token := c.GlobalString("token")
-	server := c.GlobalString("server")
+func Handle(c *cli.Context, fn HandleFunc) error {
+	var (
+		server = c.GlobalString("server")
+		token  = c.GlobalString("token")
+
+		client umschlag.ClientAPI
+	)
 
 	if server == "" {
 		fmt.Fprintf(os.Stderr, "Error: You must provide the server address.\n")
@@ -22,17 +26,20 @@ func Handle(c *cli.Context, fn HandleFunc) {
 	}
 
 	if token == "" {
-		fmt.Fprintf(os.Stderr, "Error: You must provide your access token.\n")
-		os.Exit(2)
+		client = umschlag.NewClient(
+			server,
+		)
+	} else {
+		client = umschlag.NewClientToken(
+			server,
+			token,
+		)
 	}
-
-	client := umschlag.NewClientToken(
-		server,
-		token,
-	)
 
 	if err := fn(c, client); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
-		os.Exit(3)
+		os.Exit(2)
 	}
+
+	return nil
 }
