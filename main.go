d6b84eab31e10ab5e0c8905affaecc4aaaaae668
Thomas Boerger <thomas@webhippie.de>
2017-01-15 22:50:00 +0100

    Integrated env autoloading

--- main.go
+++ main.go
@@ -5,15 +5,18 @@ import (
 	"os"
 	"runtime"
 
+	"github.com/joho/godotenv"
 	"github.com/umschlag/umschlag-cli/cmd"
 	"github.com/umschlag/umschlag-cli/config"
 	"github.com/urfave/cli"
-
-	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	if env := os.Getenv("UMSCHLAG_ENV_FILE"); env != "" {
+		godotenv.Load(env)
+	}
 
 	app := cli.NewApp()
 	app.Name = "umschlag-cli"
