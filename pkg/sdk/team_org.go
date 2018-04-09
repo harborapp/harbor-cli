873c59e98d9bd65114e0348115ce1951355fe022
Thomas Boerger <thomas@webhippie.de>
2018-04-09 15:21:16 +0200

    Dropped the sdk package

--- pkg/sdk/team_org.go
+++
@@ -1,8 +0,0 @@
-package sdk
-
-// TeamOrg represents a team org API response.
-type TeamOrg struct {
-	Team *Team  `json:"team,omitempty"`
-	Org  *Org   `json:"org,omitempty"`
-	Perm string `json:"perm,omitempty"`
-}
--- pkg/sdk/user_org.go
+++
@@ -1,8 +0,0 @@
-package sdk
-
-// UserOrg represents a user org API response.
-type UserOrg struct {
-	User *User  `json:"user,omitempty"`
-	Org  *Org   `json:"org,omitempty"`
-	Perm string `json:"perm,omitempty"`
-}
